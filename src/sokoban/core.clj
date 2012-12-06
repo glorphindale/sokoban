40a72a59f4d0b5c99849d7c47139ca64b2db03f2
Glorphindale <glorphindale@gmail.com>
2012-12-06 23:11:50 +0700

    Add copied code for screen manipulation.

--- src/sokoban/core.clj
+++ src/sokoban/core.clj
@@ -1,13 +1,83 @@
 (ns sokoban.core
     (:require [lanterna.screen :as s]))
 
-(defn main [screen-type]
-  (let [screen (s/get-screen screen-type)]
-    (s/in-screen screen
-      (s/put-string screen 0 0 "Welcome to Sokoban")
-      (s/put-string screen 0 1 "Press any key to exit")
-      (s/redraw screen)
-      (s/get-key-blocking screen))))
+(defrecord UI [kind])
+(defrecord World [])
+(defrecord Game [world uis input])
+
+(defn clear-screen [screen]
+  (let [blank (apply str (repeat 80 \space))]
+    (doseq [row (range 24)]
+      (s/put-string screen 0 row blank))))
+
+(defmulti draw-ui
+  (fn [ui game screen]
+    (:kind ui)))
+
+(defmethod draw-ui :start [ui game screen]
+  (s/put-string screen 0 0 "Welcome to the Sokoban!")
+  (s/put-string screen 0 1 "Press enter to win, anything else to lose."))
+
+(defmethod draw-ui :win [ui game screen]
+  (s/put-string screen 0 0 "Congratulations, you win!")
+  (s/put-string screen 0 1 "Press escape to exit, anything else to restart."))
+
+(defmethod draw-ui :lose [ui game screen]
+  (s/put-string screen 0 0 "Sorry, better luck next time.")
+  (s/put-string screen 0 1 "Press escape to exit, anything else to go."))
+
+(defn draw-game [game screen]
+  (clear-screen screen)
+  (doseq [ui (:uis game)]
+    (draw-ui ui game screen))
+  (s/redraw screen))
+
+(defmulti process-input
+  (fn [game input]
+    (:kind (last (:uis game)))))
+
+(defmethod process-input :start [game input]
+  (if (= input :enter)
+    (assoc game :uis [(new UI :win)])
+    (assoc game :uis [(new UI :lose)])))
+
+(defmethod process-input :win [game input]
+  (if (= input :escape)
+    (assoc game :uis [])
+    (assoc game :uis [(new UI :start)])))
+
+(defmethod process-input :lose [game input]
+  (if (= input :escape)
+    (assoc game :uis [])
+    (assoc game :uis [(new UI :start)])))
+
+(defn get-input [game screen]
+  (assoc game :input (s/get-key-blocking screen)))
+
+(defn run-game [game screen]
+  (loop [{:keys [input uis] :as game} game]
+    (when-not (empty? uis)
+      (draw-game game screen)
+      (if (nil? input)
+        (recur (get-input game screen))
+        (recur (process-input (dissoc game :input) input))))))
+
+(defn new-game []
+  (new Game
+       (new World)
+       [(new UI :start)]
+       nil))
+
+(defn main 
+  ([screen-type] (main screen-type false))
+  ([screen-type block?]
+    (letfn [(go []
+              (let [screen (s/get-screen screen-type)]
+                (s/in-screen screen
+                              (run-game (new-game) screen))))]
+    (if block?
+      (go)
+      (future (go))))))
 
 (defn -main [& args]
   (let [args (set args)
@@ -15,5 +85,5 @@
                       (args ":swing") :swing
                       (args ":text")  :text
                       :else           :auto)]
-    (main screen-type)))
+    (main screen-type true)))
 
