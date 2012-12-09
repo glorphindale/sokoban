015ae5d8ad8bb879a7615feddec2d44e28142588
Glorphindale <glorphindale@gmail.com>
2012-12-09 12:07:00 +0700

    Refactor the code.

--- src/sokoban/logic.clj
+++ src/sokoban/logic.clj
@@ -3,7 +3,7 @@
 (defrecord Game [world input continue player-pos])
 
 (defn new-world []
-  ["#####" "#...#" "#...#" "#...#" "#####"])
+  ["#####" "#...#" "#...#" "#.sz#" "#####"])
 
 (defn new-game []
   (new Game (new-world) nil [true] [1 1]))
@@ -18,9 +18,22 @@
 (defn offset-coords [[x y] [dx dy]]
   [(+ x dx) (+ y dy)])
 
+(defn get-dest [world [x y]]
+  (if (or (< x 0) (< y 0))
+    \#
+    (nth (nth world x) y)))
+
+(defn can-move? [world player-pos dir]
+  (let [dest-pos (offset-coords (dir-to-offset dir) player-pos)
+        dest-content (get-dest world dest-pos)]
+    (case dest-content
+      \# false
+      \s true
+      \. true
+      false)))
+  
 (defn move-player [player-pos world dir]
-  (let [[new-x new-y] (offset-coords (dir-to-offset dir) player-pos)
-        dest-content (nth (nth world new-y) new-x)]
-    (if (= dest-content \#)
-      player-pos
-      [new-x new-y])))
+  (let [dest-pos (offset-coords (dir-to-offset dir) player-pos)]
+    (if (can-move? world player-pos dir)
+      dest-pos
+      player-pos)))
